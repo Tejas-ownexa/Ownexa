c9b7dfb533cafefcc008be8e32aea7d8d7a90ea2
devenownexa <deven@ownexa.com>
2025-08-30 13:11:19 -0400

    Fix ESLint error - remove unused properties reference in useMemo dependency array

--- frontend/src/pages/Rentals.jsx
+++ frontend/src/pages/Rentals.jsx
@@ -111,7 +111,7 @@ const Rentals = () => {
          payments: payments
        };
     });
-  }, [tenants, properties, rentRoll]);
+  }, [tenants, rentRoll]);
 
   // Filter and sort lease entries
   const filteredLeases = React.useMemo(() => {
